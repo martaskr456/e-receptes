746bdd846028364558b2b28fe6eafb1d5340b557
Ivo Aļļēns <ivockis2004@gmail.com>
2024-06-18 08:10:34 +0300

    Apraksts par veiktajām izmaiņām

--- database/migrations/2024_06_15_141652_create_recipes_table.php
+++ database/migrations/2024_06_15_141652_create_recipes_table.php
@@ -4,32 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRecipesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
-            $table->text('description');
-            $table->string('category');
-            $table->integer('difficulty');
-            $table->integer('preparation_time');
-            $table->boolean('public')->default(true);
+            $table->integer('cooking_time');
+            $table->text('ingredients');
+            $table->text('instructions');
+            $table->foreignId('category_id')->constrained();
+            $table->boolean('is_private')->default(false);
+            $table->foreignId('user_id')->constrained();
+            $table->string('image');
             $table->timestamps();
         });
     }
 
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('recipes');
     }
-};
+}
